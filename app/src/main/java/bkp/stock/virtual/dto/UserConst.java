4e8012021ecc3a81aa8e52255750f954e9f7e3d1
Satya <75208415+Satya-Chandana@users.noreply.github.com>
2023-10-19 01:15:19 +0530

    delete

--- app/src/main/java/bkp/stock/virtual/dto/UserConst.java
+++
@@ -1,8 +0,0 @@
-package bkp.stock.virtual.dto;
-
-public class UserConst {
-    public static String user = "user";
-    public static String admin = "admin";
-    public static String teacher = "teacher";
-    public static String type = "type";
-}
--- app/src/main/java/bkp/stock/virtual/interfaces/NavListner.java
+++
@@ -1,7 +0,0 @@
-package bkp.stock.virtual.interfaces;
-/*
-* its interface which is used as click listner bitween adapter and activities
-* */
-public interface NavListner {
-    public void onItemClick1(int position);
-}
